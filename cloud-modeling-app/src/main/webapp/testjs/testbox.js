f79ceedf226c16a59b840157c1ef651297b99f27
Igor <nesterione@gmail.com>
2014-03-13 10:16:25 +0300

    Made simple example with Threejs

---
+++ cloud-modeling-app/src/main/webapp/testjs/testbox.js
@@ -0,0 +1,29 @@
+{
+
+    "metadata" : {
+		"formatVersion" : 3,
+		"description"	: "3D assets and textures for ROME - 3 Dreams of Black - at http://ro.me are licensed under a Creative CommonsAttribution-NonCommercial-ShareAlike 3.0 Unported License ( http://creativecommons.org/licenses/by-nc-sa/3.0/ )."
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+    	"DbgColor" : 15658734,
+    	"DbgIndex" : 0,
+    	"DbgName" : "default",
+    	"vertexColors" : false
+    	}],
+
+    "vertices": [0,0,0,  1,0,0,  1,1,0,   0,1,0,   0,0,1,  1,0,1,   1,1,1,  0,1,1 ],
+
+	"morphTargets": [],
+	
+    "normals": [],
+
+    "colors": [],
+
+    "uvs": [[]],
+	
+    "faces": [2, 0,1,3,  3,1,2,  0,7,4,  0,3,7,  5,7,4,  5,6,7,  1,6,5,  1,2,6,  2,7,6,  2,3,7,  1,4,5,  1,0,4]
+
+}
